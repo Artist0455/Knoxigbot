8eebe8012b1596db0ec352a31c7c2cd3db0a33cb
Artist0455 <mrsuperunboxing90@gmail.com>
2025-09-04 17:44:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from telegram.ext import ApplicationBuilder
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+from bot.handlers.start_handler import start_handler
+from bot.handlers.message_handler import message_save_handler
+from bot.handlers.history_handler import allhistory_handler
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(start_handler)
+    app.add_handler(message_save_handler)
+    app.add_handler(allhistory_handler)
+
+    print("🚀 Knox Bot is running...")
+    app.run_polling()
